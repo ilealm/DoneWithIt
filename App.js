1286db0406e755e2c87629dcf8a5c0ad37a2ec4e
Iris Leal <iris_leal_m@hotmail.com>
2021-02-10 11:10:21 -0800

    First commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { View, Text } from 'react-native';
+
+import Welcome from './screens/Welcome';
+
 
 export default function App() {
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    // <View
+    //   style={ {
+    //     backgroundColor:"#fff",
+    //     flex: 1,
+    //     flexDirection:"row",
+    //     // justifyContent:"center", // main
+    //     // alignItems:"center", // secondary        
+    //   } }
+    // >      
+    
+    <View>
+      <Welcome/>
     </View>
+
+    // </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
