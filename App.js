966bb7175b8835a551270707964fd76705dab624
Iris Leal <iris_leal_m@hotmail.com>
2021-02-11 08:33:53 -0800

    Added a component module to encapsulate the text syles.

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, Text } from 'react-native';
+
 import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
+import AppText from './app/components/AppText';
+
+
 
 export default function App() {
   
   return (
 		// <WelcomeScreen/> //automatically imported on the top
-    <ViewImageScreen/>
+    // <ViewImageScreen/>
+    <View
+      style={{
+        flex:1,   // this center the objs on the next view
+        justifyContent: "center",
+        alignItems:"center", 
+      }} >
+        <AppText          
+        >
+          I love React Native this is my first react native app! Here is so more text
+        </AppText>
+    </View>
   );
 }
