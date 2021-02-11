dfd9eb26a4a0fe1b71954ba5d4a537cb38900aa0
Iris Leal <iris_leal_m@hotmail.com>
2021-02-10 16:45:14 -0800

    Draft improvement for ViewImageScreen

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { View, Text } from 'react-native';
+import ViewImageScreen from './app/screens/ViewImageScreen';
 import WelcomeScreen from './app/screens/WelcomeScreen';
 
 export default function App() {
   
   return (
-		<WelcomeScreen/> //automatically imported on the top
+		// <WelcomeScreen/> //automatically imported on the top
+    <ViewImageScreen/>
   );
 }
