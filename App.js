bdf3231b877fabf5d01da3a8ac45616b20cdb768
Iris Leal <iris_leal_m@hotmail.com>
2021-02-12 13:10:50 -0800

    First draft of the list detail.

--- App.js
+++ App.js
@@ -6,26 +6,30 @@ import WelcomeScreen from './app/screens/WelcomeScreen';
 import AppText from './app/components/AppText';
 import AppButton from './app/components/AppButton';
 import Card from './app/components/Card';
+import ListingDetailsScreen from './app/screens/ListingDetailsScreen';
 
 
-export default function App() {
-  
+export default function App() {  
   return (  
     
+    <ListingDetailsScreen />
+    
+///////////////////////
       // <WelcomeScreen/> 
-      <View style={{
-        backgroundColor:'#f8f4f4',
-        padding: 20,
-        paddingTop: 100,
+///////////////////////
+      // <View style={{
+      //   backgroundColor:'#f8f4f4',
+      //   padding: 20,
+      //   paddingTop: 100,
         
-      }}>
-        <Card 
-          title= "Red jacket for sale"
-          subTitle="$100"
-          image = {require("./app/assets/jacket.jpg")}
-          // image={require("./app/assets/jacket.jpg")}
-        />
-      </View>
+      // }}>
+      //   <Card 
+      //     title= "Red jacket for sale"
+      //     subTitle="$100"
+      //     image = {require("./app/assets/jacket.jpg")}
+      //     // image={require("./app/assets/jacket.jpg")}
+      //   />
+      // </View>
 
   );
 }
