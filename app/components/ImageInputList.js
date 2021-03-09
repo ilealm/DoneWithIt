6f063d4e917bc39d6f05b1ae03f42363db3af960
Iris Leal <iris_leal_m@hotmail.com>
2021-03-09 12:20:14 -0800

    Added automatic scrollView to ImageInputList

--- app/components/ImageInputList.js
+++ app/components/ImageInputList.js
@@ -2,34 +2,49 @@
  * This component displays an array of images.
  * Does callback functions to remove and add.
  */
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import React, { useRef } from 'react';
+import { View, StyleSheet, ScrollView } from 'react-native';
 
 
 import ImageImput from './ImageImput';
 
 function ImageInputList({ imageUris = [], onRemoveImage, onAddImage }) {
+  // in order to get auto scrolling, I need to implement useRef
+  const scrollView = useRef();  // returns a general reference object
+  // with scrollView.current. I can access all its methods
+
+
+
   return (
-    <View style={styles.container}>
-      {/* diaplay current images on the imageUris array */}
-      {imageUris.map(uri => (
-        <View 
-        // I need to set the key here, BC this is the container.
-          key={uri} 
-          style={styles.image} //I need to put the style here BC is not supported in the component
-          > 
-          <ImageImput 
-              imageUri={uri} 
-              
-              onChangeImage={() => onRemoveImage(uri) } 
-          /> 
-        </View>
-        ))}
-     {/* Image input to add a new image */}
-     <ImageImput
-        onChangeImage={uri => onAddImage(uri)}
-      />
-    </View>
+
+    // I need to add scrollview so if I have more images that can fix in the screen, the rest of the images are shown in the next line
+    // by default is vertical scroll bar. If I want it horizontal: horizontal={true} or just horizontal
+    <ScrollView 
+      ref={scrollView} 
+      horizontal 
+      onContentSizeChange={() => scrollView.current.scrollToEnd() } 
+    >
+      <View style={styles.container}>
+        {/* diaplay current images on the imageUris array */}
+        {imageUris.map(uri => (
+          <View 
+          // I need to set the key here, BC this is the container.
+            key={uri} 
+            style={styles.image} //I need to put the style here BC is not supported in the component
+            > 
+            <ImageImput 
+                imageUri={uri} 
+                
+                onChangeImage={() => onRemoveImage(uri) } 
+            /> 
+          </View>
+          ))}
+      {/* Image input to add a new image */}
+      <ImageImput
+          onChangeImage={uri => onAddImage(uri)}
+        />
+      </View>
+    </ScrollView>
   );
 }
 
