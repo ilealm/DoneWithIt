44d3b768e886ed2295114b415627d4c3ea2191b1
Iris Leal <iris_leal_m@hotmail.com>
2021-03-11 13:15:23 -0800

    Hide the screen name on WelcomeScreen, by adding the option in AuthNavigator

--- app/navigation/AuthNavigator.js
+++ app/navigation/AuthNavigator.js
@@ -10,7 +10,9 @@ const Stack = createStackNavigator();
 
 const AuthNavigator = () =>(
   <Stack.Navigator>
-    <Stack.Screen name="Welcome" component={WelcomeScreen} />
+    <Stack.Screen name="Welcome" 
+                  component={WelcomeScreen} 
+                  options={{headerShown:false}}/>
     <Stack.Screen name="Login" component={LoginScreen} />
     <Stack.Screen name="Register" component={RegisterScreen} />
   </Stack.Navigator>
