9571834fc1df3e206c0976c80ae5e34792fab111
Iris Leal <iris_leal_m@hotmail.com>
2021-03-11 15:35:21 -0800

    Added big middle red button WO funcionality

--- app/navigation/AppNavigator.js
+++ app/navigation/AppNavigator.js
@@ -3,18 +3,37 @@
  */
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-import ListingEditScreen from '../screens/ListingEditScreen';
-import FeedNavigator from './FeedNavigator';
 import AccountNavigator from './AccountNavigator';
+import FeedNavigator from './FeedNavigator';
+import ListingEditScreen from '../screens/ListingEditScreen';
+import NewListingButton from './NewListingButton';
 
 const Tab = createBottomTabNavigator();
 
 const AppNavigator = () => (
   <Tab.Navigator>
-    <Tab.Screen name="Feed" component={FeedNavigator} />
-    <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountNavigator} />
+    <Tab.Screen name="Feed" component={FeedNavigator} 
+        options={{
+            tabBarIcon:({ color, size }) =>  // taking the props from the obj, native will know this info
+                <MaterialCommunityIcons name="home" color={color} size={size} /> 
+                 }} 
+    />
+    <Tab.Screen name="ListingEdit" component={ListingEditScreen} 
+        options={{
+          // for having the big round center botton:
+          tabBarButton: () => <NewListingButton  />,
+          tabBarIcon:({ color, size }) =>
+              <MaterialCommunityIcons name="plus-circle" color={color} size={size} /> 
+                }} 
+    />
+    <Tab.Screen name="Account" component={AccountNavigator} 
+          options={{
+            tabBarIcon:({ color, size }) =>
+                <MaterialCommunityIcons name="account" color={color} size={size} /> 
+                  }} 
+    />
   </Tab.Navigator>
 );
 
