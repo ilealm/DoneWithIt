8c5987238c9652a37cb4c364176a42bd7c0e2f6b
Iris Leal <iris_leal_m@hotmail.com>
2021-03-15 14:23:34 -0700

    Added example of how to storage/get info in react Async Storage

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Button, View, Image, Text } from 'react-native';
+import { AsyncStorage } from 'react-native';
 // this is defined in the stack library
 import { createStackNavigator } from '@react-navigation/stack'
 // this is defined in the main navigation library
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
+import NetInfo,  { useNetInfo } from '@react-native-community/netinfo';
 
 // import AccountScreen from './app/screens/AccountScreen';
 import AuthNavigator from './app/navigation/AuthNavigator';
@@ -24,6 +26,7 @@ import AuthNavigator from './app/navigation/AuthNavigator';
 import ListingEditScreen from './app/screens/ListingEditScreen';
 import navigationTheme from './app/navigation/navigationTheme';
 import AppNavigator from './app/navigation/AppNavigator';
+import { endAsyncEvent } from 'react-native/Libraries/Performance/Systrace';
 // import TextInput from './app/components/TextInput';
 // import ViewImageScreen from './app/screens/ViewImageScreen';
 // import WelcomeScreen from './app/screens/WelcomeScreen';
@@ -32,12 +35,37 @@ import AppNavigator from './app/navigation/AppNavigator';
 
 
 export default function App() { 
+  // get info of the conection, using the method fetch which returns a promise. I can use await or .then
+  // NetInfo.fetch().then(netInfo => {console.log(netInfo)})
+  // addEventListener: subscribe to network status changes. Pass a func. that will be called everytime the network status changes
+  // The first time it returns 2 objs, and 1rst says isInternetReachable=null, then the other times says isInternetReachable=true
+  // const unsubscribe =  NetInfo.addEventListener(netInfo => console.log(netInfo))
+  // // don't forget to unsubscribe BC if not I will have memory leak 
+  // unsubscribe();
+  // but I can have all this manage using useNetInfo hook, which handles the subcribe and unsubscribe
+  // const netInfo = useNetInfo();
+  const demo = async () => {
+    try{
+      await AsyncStorage.setItem('person', JSON.stringify({id:1}));
+      // to read
+      const value = await AsyncStorage.getItem('person');
+      const person = JSON.parse(value);
+      console.log(person);
+    } 
+    catch (error) {
+      console.log(error);
+    }
+  }
+
+  demo();
+
   return ( 
-    <NavigationContainer theme={navigationTheme}>
-      {/* Depending on user auth, is what I will display */}
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
-    </NavigationContainer>
+    null
+    // <NavigationContainer theme={navigationTheme}>
+    //   {/* Depending on user auth, is what I will display */}
+    //   {/* <AuthNavigator /> */}
+    //   <AppNavigator />
+    // </NavigationContainer>
    
   );
 }
