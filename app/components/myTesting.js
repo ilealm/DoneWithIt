b8ec62080706d3ca429244aaaaebb1bf31bfc954
Iris Leal <iris_leal_m@hotmail.com>
2021-03-16 15:13:07 -0700

    Added some comments.

--- app/components/myTesting.js
+++ app/components/myTesting.js
@@ -1,54 +1,43 @@
-import React, { useEffect } from "react";
-import { FlatList, StyleSheet } from "react-native";
+import React from "react";
+import { View, StyleSheet, Modal } from "react-native";
+import * as Progress from "react-native-progress";
+import LottieView from "lottie-react-native";
 
-import ActivityIndicator from "../components/ActivityIndicator";
-import Button from "../components/Button";
-import Card from "../components/Card";
 import colors from "../config/colors";
-import listingsApi from "../api/listings";
-import routes from "../navigation/routes";
-import Screen from "../components/Screen";
-import AppText from "../components/Text";
-import useApi from "../hooks/useApi";
 
-function ListingsScreen({ navigation }) {
-  const getListingsApi = useApi(listingsApi.getListings);
-
-  useEffect(() => {
-    getListingsApi.request();
-  }, []);
-
+function UploadScreen({ onDone, progress = 0, visible = false }) {
   return (
-    <Screen style={styles.screen}>
-      {getListingsApi.error && (
-        <>
-          <AppText>Couldn't retrieve the listings.</AppText>
-          <Button title="Retry" onPress={getListingsApi.request} />
-        </>
-      )}
-      <ActivityIndicator visible={getListingsApi.loading} />
-      <FlatList
-        data={getListingsApi.data}
-        keyExtractor={(listing) => listing.id.toString()}
-        renderItem={({ item }) => (
-          <Card
-            title={item.title}
-            subTitle={"$" + item.price}
-            imageUrl={item.images[0].url}
-            onPress={() => navigation.navigate(routes.LISTING_DETAILS, item)}
-            thumbnailUrl={item.images[0].thumbnailUrl}
+    <Modal visible={visible}>
+      <View style={styles.container}>
+        {progress < 1 ? (
+          <Progress.Bar
+            color={colors.primary}
+            progress={progress}
+            width={200}
+          />
+        ) : (
+          <LottieView
+            autoPlay
+            loop={false}
+            onAnimationFinish={onDone}
+            source={require("../assets/animations/done.json")}
+            style={styles.animation}
           />
         )}
-      />
-    </Screen>
+      </View>
+    </Modal>
   );
 }
 
 const styles = StyleSheet.create({
-  screen: {
-    padding: 20,
-    backgroundColor: colors.light,
+  animation: {
+    width: 150,
+  },
+  container: {
+    alignItems: "center",
+    flex: 1,
+    justifyContent: "center",
   },
 });
 
-export default ListingsScreen;
+export default UploadScreen;
